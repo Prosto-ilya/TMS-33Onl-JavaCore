1785914076b9d0ea35602acae14189d04e992718
Ilya <ilya.alefirenko2004@gmail.com>
2025-02-09 16:31:26 +0300

    Files

---
+++ src/Lesson_14_homework/Validation/Validation.java
@@ -0,0 +1,46 @@
+package Lesson_14_homework.Validation;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class Validation {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите путь к входному файлу: ");
+        String inputFilePath = scanner.nextLine();
+
+        List<String> validDocuments = new ArrayList<>();
+        List<String> invalidDocuments = new ArrayList<>();
+
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(inputFilePath));
+            for (String line : lines) {
+                String docNumber = line.trim();
+                if (isValidDocumentNumber(docNumber)) {
+                    validDocuments.add(docNumber);
+                } else {
+                    String reason = "Должен содержать 15 символов и начинаться с 'docnum' или 'contract'";
+                    invalidDocuments.add(docNumber + " - " + reason);
+                }
+            }
+
+            Files.write(Paths.get(
+                    "C:\\Java_homework\\Lesson3_Homework\\src\\Lesson_14_homework\\Validation\\valid_document.txt"),
+                    validDocuments);
+            Files.write(Paths.get(
+                    "C:\\Java_homework\\Lesson3_Homework\\src\\Lesson_14_homework\\Validation\\invalid_document.txt"),
+                    invalidDocuments);
+
+            System.out.println("Обработка завершена. Результаты записаны.");
+        } catch (IOException e) {
+            System.err.println("Ошибка при обработке файла: " + e.getMessage());
+        }
+
+    }
+    private static boolean isValidDocumentNumber(String docNumber) {
+        return docNumber.length() == 15 &&
+                (docNumber.startsWith("docnum") || docNumber.startsWith("contract"));
+    }
+}
---
+++ src/Lesson_14_homework/longestWordInFile/longestWord.java
@@ -0,0 +1,25 @@
+package Lesson_14_homework.longestWordInFile;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class longestWord {
+    public static void main(String[] args) throws IOException {
+        String input = "C:\\Java_homework\\Lesson3_Homework\\src\\Lesson_14_homework\\longestWordInFile\\txt.txt";
+        String output = "C:\\Java_homework\\Lesson3_Homework\\src\\Lesson_14_homework\\longestWordInFile\\txt1.txt";
+        try{
+            String text=new String(Files.readAllBytes(Path.of(input)));
+            String longestWord = "";
+
+            for(String word :text.split("\\W+")){
+                if(word.length()> longestWord.length()){
+                    longestWord=word;
+                }
+            }
+            Files.write(Path.of(output), longestWord.getBytes());
+            System.out.println("The longest word is " + longestWord + " and this word write to file txt1");
+        }catch (Exception e){
+            System.err.println("Error"+ e.getMessage());
+        }
+    }
+}
